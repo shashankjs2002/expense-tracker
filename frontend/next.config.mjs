366da20f3975e56ec9094f7535cf36cd2fb7a75c
Shashank <shashankjs2002@gmail.com>
2026-04-26 11:22:18 +0530

    configs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:4000/api/:path*',
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
